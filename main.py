eaa11a1286be6987aa4d4e164e684c8c360929a8
Nizobe <oshasnan@yahoo.com>
2020-11-24 13:26:12 -0500

    first working copy that counts number of cards in a json file. ignored renamed file. ignored python virtualenv directory

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import json
 
-f = open 
+f = open('default_cards.json', encoding='utf8')
+
+data = json.load(f)
+
+keyCount = 0
+cardCount = 0
+stop = 0
+
+for entry in data:
+    for(k, v) in entry.items():
+        keyCount += 1
+        if k == "object" and v == "card":
+            cardCount += 1
+
+        if stop < 10:
+            print(k, "\n", v, "\n")
+            stop += 1
+
+print(F"keyCount: 1", keyCount)
+print(F"cardCount: 1", cardCount)
+f.close()
