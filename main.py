d183c66052af34a6666d038aa0185c764791de27
Nizobe <oshasnan@yahoo.com>
2020-11-26 12:26:09 -0500

    stuck trying to iterate through values of a given printing...

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from operator import itemgetter
 
 #"data" is a list
 #entry is a dict
@@ -11,10 +12,27 @@ def build_oracle_dict(dataset):
     for entry in dataset:
         oracle_id = entry["oracle_id"]
         if oracle_id in oracle_dict:
-            oracle_dict[oracle_id] = [oracle_dict[oracle_id], entry]
+            oracle_dict[oracle_id] = [oracle_dict[oracle_id].copy(), entry.copy()]
         else:
-            oracle_dict[oracle_id] = entry
+            oracle_dict[oracle_id] = entry.copy()
     return oracle_dict
+
+
+def count_sets(oracle_dict):
+    card_sets = dict()
+    for card in oracle_dict:
+        for printing in oracle_dict[card]: #do i need to go one level deeper...?
+            card_set = printing["set_name"]
+            if card_set in card_sets:
+                card_sets[card_set] += 1
+            else:
+                card_sets[card_set] = 1
+
+    card_count = 0
+    for card in card_sets.values():
+        card_count += 1
+    print(F"There are {len(card_sets.keys())} sets of cards.\nTotaling {card_count} cards.")
+    return card_sets
 
 
 def extract_oracle_ids(dataset):
@@ -32,6 +50,9 @@ def main():
     oracle_dict = build_oracle_dict(card_data)
 
     print(F"build_oracle_dict() found # oracle_id values: {len(oracle_dict.keys())}")
+
+    count_sets(oracle_dict)
+
     card_file.close()
     return 0
 
